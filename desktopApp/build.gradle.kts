a3e2c59ec9ce9e4a6b13e6b6729cd886a000cbbc
equationl <equationl@qq.com>
2023-10-24 16:33:18 +0800

    增加桌面端 jni 自动编译

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.apache.tools.ant.taskdefs.condition.Os
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
@@ -28,5 +29,86 @@ compose.desktop {
 
             appResourcesRootDir.set(project.layout.projectDirectory.dir("resources"))
         }
+    }
+}
+
+tasks.register("compileJni") {
+    doLast {
+        val resourcePath = File(rootProject.projectDir, "desktopApp/resources/common/lib/")
+        val binFilePath = File(resourcePath, "nativelib.bin")
+        val cppFilePath = File(rootProject.projectDir, "nativelib/src/main/cpp/nativelib.cpp")
+
+
+        println("compile jni for desktop running……")
+
+        val jdkFile = org.gradle.internal.jvm.Jvm.current().javaHome
+        val systemPrefix: String
+
+        if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+            systemPrefix = "win32"
+        }
+        else if (Os.isFamily(Os.FAMILY_MAC)) {
+            // TODO 需要确定
+            systemPrefix = ""
+        }
+        else {
+            println("UnSupport System for compiler cpp, please compiler manual")
+            return@doLast
+        }
+
+        val includePath1 = jdkFile.resolve("include")
+        val includePath2 = includePath1.resolve(systemPrefix)
+
+        println("Check Desktop Resources Path……")
+
+        if (!resourcePath.exists()) {
+            println("${resourcePath.absolutePath} not exists, create……")
+            mkdir(resourcePath)
+        }
+
+        val runTestResult = runCommand("g++ --version")
+        if (!runTestResult.first) {
+            println("Error: Not find command g++, Please install it and add to your system environment path")
+            println(runTestResult.second)
+            return@doLast
+        }
+
+        val command = "g++ ${cppFilePath.absolutePath} -o ${binFilePath.absolutePath} -shared -fPIC -I ${includePath1.absolutePath} -I ${includePath2.absolutePath}"
+
+        println("running command $command……")
+
+        val compilerResult = runCommand(command)
+
+        if (!compilerResult.first) {
+            println("Command run fail: ${compilerResult.second}")
+            return@doLast
+        }
+
+        println(compilerResult.second)
+
+        println("compile jni for desktop all done")
+
+    }
+}
+
+tasks.named("compileKotlinJvm") {
+    dependsOn("compileJni")
+}
+
+fun runCommand(command: String, timeout: Long = 120): Pair<Boolean, String> {
+    val process = ProcessBuilder()
+        .command(command.split(" "))
+        .directory(rootProject.projectDir)
+        .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+        .redirectError(ProcessBuilder.Redirect.INHERIT)
+        .start()
+    process.waitFor(timeout, TimeUnit.SECONDS)
+    val result = process.inputStream.bufferedReader().readText()
+    val error = process.errorStream.bufferedReader().readText()
+    return if (error.isBlank()) {
+        Pair(true, result)
+    }
+    else {
+        Pair(false, error)
     }
 }
