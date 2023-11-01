24f00ba473725dc5b19ee6203cfed8354f546fc8
equationl <equationl@qq.com>
2023-11-01 15:31:56 +0800

    优化 desktop jni 编译任务

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -1,4 +1,4 @@
-import org.apache.tools.ant.taskdefs.condition.Os
+import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
@@ -33,65 +33,79 @@ compose.desktop {
 }
 
 tasks.register("compileJni") {
+    description = "compile jni binary file for desktop"
+
+    val resourcePath = File(rootProject.projectDir, "desktopApp/resources/common/lib/")
+    val binFilePath = File(resourcePath, "nativelib.bin")
+    val cppFileDirectory = File(rootProject.projectDir, "nativelib/src/main/cpp")
+    val cppFilePath = File(cppFileDirectory, "nativelib.cpp")
+
+    // 指定输入、输出文件，用于增量编译
+    inputs.dir(cppFileDirectory)
+    outputs.file(binFilePath)
+
     doLast {
-        val resourcePath = File(rootProject.projectDir, "desktopApp/resources/common/lib/")
-        val binFilePath = File(resourcePath, "nativelib.bin")
-        val cppFilePath = File(rootProject.projectDir, "nativelib/src/main/cpp/nativelib.cpp")
-
-
-        println("compile jni for desktop running……")
+        project.logger.info("compile jni for desktop running……")
 
         val jdkFile = org.gradle.internal.jvm.Jvm.current().javaHome
         val systemPrefix: String
 
-        if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+        val os: OperatingSystem = DefaultNativePlatform.getCurrentOperatingSystem()
+
+        if (os.isWindows) {
             systemPrefix = "win32"
         }
-        else if (Os.isFamily(Os.FAMILY_MAC)) {
+        else if (os.isMacOsX) {
             systemPrefix = "darwin"
         }
+        else if (os.isLinux) {
+            systemPrefix = "linux"
+        }
         else {
-            println("UnSupport System for compiler cpp, please compiler manual")
+            project.logger.error("UnSupport System for compiler cpp, please compiler manual")
             return@doLast
         }
 
         val includePath1 = jdkFile.resolve("include")
         val includePath2 = includePath1.resolve(systemPrefix)
 
-        println("Check Desktop Resources Path……")
+        if (!includePath1.exists() || !includePath2.exists()) {
+            val msg = "ERROR: $includePath2 not found!\nMaybe it's because you are using JetBrain Runtime (Jbr)\nTry change Gradle JDK to another jdk which provide jni support"
+            throw GradleException(msg)
+        }
+
+        project.logger.info("Check Desktop Resources Path……")
 
         if (!resourcePath.exists()) {
-            println("${resourcePath.absolutePath} not exists, create……")
+            project.logger.info("${resourcePath.absolutePath} not exists, create……")
             mkdir(resourcePath)
         }
 
         val runTestResult = runCommand("g++ --version")
         if (!runTestResult.first) {
-            println("Error: Not find command g++, Please install it and add to your system environment path")
-            println(runTestResult.second)
-            return@doLast
+            throw GradleException("Error: Not find command g++, Please install it and add to your system environment path\n${runTestResult.second}")
         }
 
         val command = "g++ ${cppFilePath.absolutePath} -o ${binFilePath.absolutePath} -shared -fPIC -I ${includePath1.absolutePath} -I ${includePath2.absolutePath}"
 
-        println("running command $command……")
+        project.logger.info("running command $command……")
 
         val compilerResult = runCommand(command)
 
         if (!compilerResult.first) {
-            println("Command run fail: ${compilerResult.second}")
-            return@doLast
+            throw GradleException("Command run fail: ${compilerResult.second}")
         }
 
-        println(compilerResult.second)
+        project.logger.info(compilerResult.second)
 
-        println("compile jni for desktop all done")
-
+        project.logger.lifecycle("compile jni for desktop all done")
     }
 }
 
-tasks.named("compileKotlinJvm") {
-    dependsOn("compileJni")
+gradle.projectsEvaluated {
+    tasks.named("prepareAppResources") {
+        dependsOn("compileJni")
+    }
 }
 
 fun runCommand(command: String, timeout: Long = 120): Pair<Boolean, String> {
