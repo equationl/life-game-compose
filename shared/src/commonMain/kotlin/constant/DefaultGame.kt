a470eabd16c7b0c45c766898c0a8cefca2683ecc
equationl <equationl@qq.com>
2023-11-02 15:03:32 +0800

    加了几个预设

--- shared/src/commonMain/kotlin/constant/DefaultGame.kt
+++ shared/src/commonMain/kotlin/constant/DefaultGame.kt
@@ -1,5 +1,9 @@
 package constant
 
 enum class DefaultGame(val showName: String, val loadName: String) {
-    Bomber(showName = "bomber", loadName = "bomber.txt")
+    Bomber(showName = "bomber", loadName = "bomber.txt"),
+    Sidecar(showName = "sidecar", loadName = "sidecar.txt"),
+    Siesta(showName = "siesta", loadName = "siesta.txt"),
+    SkewedTrafficLight(showName = "skewed_traffic_light", loadName = "skewed_traffic_light.txt"),
+    TNosed(showName = "t_nosed", loadName = "t_nosed.txt"),
 }
